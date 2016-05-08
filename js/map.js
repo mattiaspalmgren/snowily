0508840f53da8fa4beadab23f626cf998a3677ed
mattiaspalmgren <mattiaspalmgren92@gmail.com>
2016-05-08 20:20:49 +0200

    zoom works as expected

--- js/map.js
+++ js/map.js
@@ -1,8 +1,9 @@
 var d3 = require('d3');
-var geojson = require('geojson');
 var q = require('d3-queue');
 var topojson = require('topojson');
 var $ = require('jquery');
+snowtypes = require('./snowtypes');
+
 
 var width = $("#map").width(),
     height = $("#map").height(),
@@ -28,19 +29,19 @@ var map = svg.append("g");
 
 var initMap = function() {
 
-    
     q.queue()
         .defer(d3.json, "data/griments.json")
         .defer(d3.json, "data/resorts.json")
         .defer(d3.json, "data/ch-country.json")
         .await(ready);
 
-    function ready(error, griments, resorts, country) {
+    function ready(error, grimentsStatic, resorts, country) {
       if (error) throw error;
 
-        var griments = griments[0];
+        griments = grimentsStatic[0];
         var edgeArr = createEdgeArray(griments.edges, griments.vertices);
-        
+        var snowArr = snowtypes.getResortWithSnowTypes(griments);
+
         map.selectAll(".country")
           .data(topojson.feature(country, country.objects.country).features)
         .enter().append("path")
@@ -48,6 +49,16 @@ var initMap = function() {
           .attr("d", path)
           .style("fill", "#DCEAF2")
           .style("stroke", "black");
+
+        map.selectAll("resorts")
+            .data(resorts).enter()
+            .append("rect")
+            .attr("class", "resort")
+            .attr("x", function (d) { return projection(d.lonLat)[0]; })
+            .attr("y", function (d) { return projection(d.lonLat)[1]; })
+            .attr("width", 10)
+            .attr("height", 10)
+            .on("click", clicked);
 
         map.selectAll("line")
             .data(edgeArr)
@@ -57,68 +68,69 @@ var initMap = function() {
             .attr("y1", function(d) { return d.start[1]})   
             .attr("x2", function(d) { return d.end[0]})     
             .attr("y2", function(d) { return d.end[1]})
-            .attr("stroke-width", 2)
-            .attr("stroke", "#91AA9D");
-
-        map.selectAll("resorts")
-            .data(resorts).enter()
-            .append("rect")
-            .attr("x", function (d) { return projection(d.lonLat)[0]; })
-            .attr("y", function (d) { return projection(d.lonLat)[1]; })
-            .attr("width", 10)
-            .attr("height", 10)
-            .on("click", clicked);
+            .attr("stroke-width", 0.5)
+            .attr("stroke", "#91AA9D")
+            .style("opacity", 0);
 
         map.selectAll("rects")
-            .data(griments.vertices).enter()
-            .append("rect")
-            .attr("class", function(d) { return d.name})
-            .attr("x", function (d) { return projection(d.lonLat)[0]-0.0625; })
-            .attr("y", function (d) { return projection(d.lonLat)[1]-0.0625; })
-            .attr("width", 10)
-            .attr("height", 10)
-            .on("click", clicked);
+           .data(griments.vertices).enter()
+           .append("rect")
+           .attr("class", "grimentz")
+           .attr("x", function (d) { return projection(d.lonLat)[0]-0.0625; })
+           .attr("y", function (d) { return projection(d.lonLat)[1]-0.0625; })
+           .attr("fill", function (d) { return d.snow.color})
+           .attr("width", 0.1)
+           .attr("height", 0.1)
+           .style("opacity", 0)
+           .on("click", clicked);
+
+
+            function clicked(d) {
+                var x, y, k, dur, op;
+                
+                if (d && centered !== d) {
+                    var centroid = projection(d.lonLat);
+                    x = centroid[0];
+                    y = centroid[1];
+                    k = 80;
+                    centered = d;
+                } else {
+                    x = width / 2;
+                    y = height / 2;
+                    k = 1;
+                    centered = null;
+                }
+
+                op = (k == 80) ? 1 : 0;
+               
+                map.selectAll(".grimentz")
+                    .transition()
+                    .duration(750)
+                    .style("opacity", op);
+
+                map.selectAll("line")
+                    .transition()
+                    .duration(750)
+                    .style("opacity", op);
+
+                map.selectAll(".resort")
+                    .transition()
+                    .duration(750)
+                    .style("opacity", function() {if(op){return 0} else{return 1} });
+                
+                map.transition()
+                  .duration(750)
+                  .ease("cubic-in-out")
+                  .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
+                  .style("stroke-width", 1.5 / k + "px");
+
+            };        
 
     }
 
 }
 
 // Help functions
-
-function clicked(d) {
-    var x, y, k, dur;
-    if (d && centered !== d) {
-        var centroid = projection(d.lonLat);
-        x = centroid[0];
-        y = centroid[1];
-        k = 80;
-        centered = d;
-    } else {
-        x = width / 2;
-        y = height / 2;
-        k = 1;
-        centered = null;
-    }
-
-    if(k == 80) { w = 0.2; dur = 500 } else { w = 10; dur = 1000 }
-    map.selectAll("rect").transition()
-        .duration(dur)
-        .ease("cubic-in-out")
-        .attr("width", w)
-        .attr("height", w);
-
-    map.selectAll("line").transition()
-        .duration(dur)
-        .ease("cubic-in-out")
-        .attr("stroke-width", 0.6)
-
-    map.transition()
-      .duration(750)
-      .ease("cubic-in-out")
-      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
-      .style("stroke-width", 1.5 / k + "px");
-
-};
 
 function findVertex(arr, el) {
     for (var i = 0; i < arr.length; i++) {
---
+++ js/snowtypes.js
@@ -0,0 +1,11 @@
+var snowtypes = [{"id":0, "name": "powder", "color": "#D1DBBD"}, {"id":1, "name": "crud", "color" : "#FCFFF5"}, {"id":2, "name": "crust", "color": "#3E606F"}, {"id":3, "name": "powder", "color" : "#193441"}];
+
+function getResortWithSnowTypes(resort) {
+	for (var i = 0; i < resort.vertices.length; i++) {
+		resort.vertices[i].snow = snowtypes[Math.floor((Math.random() * snowtypes.length))];
+	}
+	console.log(resort);
+	return resort;
+}
+
+module.exports['getResortWithSnowTypes'] = getResortWithSnowTypes;
