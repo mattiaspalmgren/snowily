83851dca0cea56a2b5286fcd8c6c0ae02c6d72d4
mattiaspalmgren <mattiaspalmgren92@gmail.com>
2016-05-29 12:04:10 +0200

    added pseudocode for snow classification

--- js/snowtypes.js
+++ js/snowtypes.js
@@ -1,10 +1,26 @@
 var snowtypes = [{"id":0, "name": "powder", "color": "#FCFFF5"}, {"id":1, "name": "crud", "color" : "#ABADA6"}, {"id":2, "name": "crust", "color": "#EFECCA"}, {"id":3, "name": "slush", "color" : "#EFEC96"}];
-// var snowtypes = [{"id":0, "name": "powder", "color": "#FCFFF5"}, {"id":1, "name": "crud", "color" : "#D1DBBD"}, {"id":2, "name": "crust", "color": "#91AA9D"}, {"id":3, "name": "slush", "color" : "#3E606F"}];
 
 function getResortWithSnowTypes(resort) {
 	for (var i = 0; i < resort.vertices.length; i++) {
 		resort.vertices[i].snow = snowtypes[Math.floor((Math.random() * snowtypes.length))];
 	}
+
+  // PSEUDO-CODE FOR SNOW CLASSIFICATION
+  // var weather = getWeather(resort.vertices[i].lonLat);
+  // if(weather.newSnow > 10 && (todayDate - weather.newSnow.date) < 2) && weather.temperature < 0 && !(weather.beenWarm)) {
+  //   //Assign powder
+  //   resort.vertices[i].snow = snowtypes[0];
+  // } else if ((todayDate - weather.newSnow.date) > 2) && weather.temperature < 0 && !(weather.beenWarm))) {
+  //   //Assign crud
+  //   resort.vertices[i].snow = snowtypes[1];
+  // } else if (weather.hasBeenSunny && weather.hasBeenWindy && weather.newSnow < 2 && weather.temperature < 0) {
+  //   //Assign crust
+  //   resort.vertices[i].snow = snowtypes[2];
+  // } else {
+  //   //Assign slush
+  //   resort.vertices[i].snow = snowtypes[3];
+  // };
+
 	return resort;
 }
 
@@ -75,6 +91,7 @@ function createEdgeArray(edgeArr, vertexArr, projection) {
         
         var newSnowId;
         if(endSnowId > startSnowId){newSnowId = startSnowId+i} else {newSnowId = startSnowId-i};
+        // Here the face of the edge would effect the snowType in combination with the weather
         segments.push({"start": [oldX, y(oldX)], "end": [newX, y(newX)], "snow": snowtypes[newSnowId]});
       }
 
